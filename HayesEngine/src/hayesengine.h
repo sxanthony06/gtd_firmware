ee83c0f855ed2f03aa27f4ee641c28a17db7d8a8
Stephen A. Anthony <sxanthony06@gmail.com>
2022-06-20 14:25:38 -0400

    Added poll_for_async_response in hayesengine implementation to wait for asynchronous responses of GSM module. Changed sim5320 implemetation to use aforementioned function to confirm completion of AT command before continuining with program.

--- HayesEngine/src/hayesengine.h
+++ HayesEngine/src/hayesengine.h
@@ -4,24 +4,27 @@
 #define MAX_SERIAL_BAUDRATE 115200
 #define MIN_SERIAL_BAUDRATE 4800
 
-#define YIELD_DURATION_MULTIPLIER 10
-
 class HayesEngine {
     private:
         HardwareSerial* serial_device = NULL;
-        unsigned char* buffer;
-        uint16_t buffer_size = 0;
-        uint8_t yield_duration_in_us = ((float)1/MIN_SERIAL_BAUDRATE)*1000*1000;
+        char* buf;
+        uint16_t buf_size = 0;	
+	size_t buf_index = 0;	
+        uint8_t yield_duration_in_ms = 0;
+        
+        size_t raw_bytes_sent(uint16_t wait_time, size_t attempts);
+        int16_t ingest_serial_dev_initial_response(void);
+        
+        
     public:
-	HayesEngine(HardwareSerial& serial_dev, uint16_t buffer_size);
+	HayesEngine(HardwareSerial& serial_dev, uint16_t len, uint8_t yield_duration);
         ~HayesEngine();
 	
-        void execute_at_command(const char *const cmd, uint16_t wait_time);
-        uint8_t establish_module_comms(const uint32_t* baudrates, uint8_t amount_possible_rates, uint32_t preffered_baudrate);
-        bool set_buffer_size(uint16_t size);
-        size_t pipe_raw_input(uint8_t* buf, size_t s, uint16_t wait_time);
-        size_t read_cmd_response(uint16_t wait_time);
-        size_t read_response_raw_input(uint16_t wait_time, uint8_t attempts);
-        const char *const get_buffer_content(void);
+        void execute_at_command(const char *const cmd);
+        int8_t establish_module_comms(const uint32_t* baudrates, size_t amount_possible_rates, uint32_t preffered_baudrate);
+        const char *const buf_content(void);
+        bool set_buf_size(size_t size);
         
+        size_t pipe_raw_input(const uint8_t* tmp_buf, size_t s);
+        int16_t poll_for_async_response(uint16_t wait_time);
 };
